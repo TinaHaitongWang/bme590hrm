4bf9af0a6b1b7984089ed739fd179591d569b39e
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-21 00:37:08 -0400

    add file dataValidation.py

---
+++ dataValidation.py
@@ -0,0 +1,14 @@
+from readCSV import importData
+import pandas as pd
+
+def is_data_number(test_data):
+    # remove nan/ empty
+    print(test_data)
+    new_data = test_data[pd.to_numeric(test_data.time, errors= 'coerce').notnull()]
+    test_data2 = new_data[pd.to_numeric(new_data.voltage, errors='coerce').notnull()]
+    print(test_data2)
+
+if __name__ == '__main__':
+    filename = "test_data30.csv"
+    test_data = importData(filename)
+    is_data_number(test_data)
