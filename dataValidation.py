125fa82b70caa1b91ab0938b0347ffa1a33f8fcf
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-21 00:48:19 -0400

    add dataValidation.py file

--- dataValidation.py
+++ dataValidation.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 def is_data_number(test_data):
     # remove nan/ empty
-    print(test_data)
+    # print(test_data)
     new_data = test_data[pd.to_numeric(test_data.time, errors= 'coerce').notnull()]
     test_data2 = new_data[pd.to_numeric(new_data.voltage, errors='coerce').notnull()]
-    print(test_data2)
+    # print(test_data2)
+    return test_data2
 
 if __name__ == '__main__':
-    filename = "test_data30.csv"
+    filename = "test_data32.csv"
     test_data = importData(filename)
     is_data_number(test_data)
---
+++ test_dataValidation.py
@@ -0,0 +1,21 @@
+from dataValidation import is_data_number
+from readCSV import importData
+import pytest
+import pandas as pd
+
+@pytest.mark.parametrize("input,expected",[
+    ("test_data1.csv", 10000),
+    ("test_data5.csv", 10000),
+    ("test_data17.csv", 10000),
+    ("test_data23.csv", 10000),
+    ("test_data28.csv", 9998),
+    ("test_data29.csv", 9998),
+    ("test_data31.csv", 9996),
+    ("test_data32.csv", 10000),
+])
+
+def test_is_data_number(input,expected):
+    testdata = importData(input)
+    output = is_data_number(testdata)
+    len = output.shape[0]
+    assert len == expected
