7d9c49cc3dd6bd80d0c489bce0e107434fd6e521
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-21 02:52:53 -0400

    fix pep8 issue

--- dataValidation.py
+++ dataValidation.py
@@ -5,8 +5,10 @@ import pandas as pd
 def is_data_number(test_data):
     # remove nan/ empty
     # print(test_data)
-    new_data = test_data[pd.to_numeric(test_data.time, errors='coerce').notnull()]
-    test_data2 = new_data[pd.to_numeric(new_data.voltage, errors='coerce').notnull()]
+    new_data = test_data[pd.to_numeric(test_data.time,
+                                       errors='coerce').notnull()]
+    test_data2 = new_data[pd.to_numeric(new_data.voltage,
+                                        errors='coerce').notnull()]
     # print(test_data2)
     return test_data2
 
