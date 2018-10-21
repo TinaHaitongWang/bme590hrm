132672bf77f4dbb50c9d8850bcf2c313eb83f0c1
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-20 22:45:10 -0400

    revise the readCSV file

--- readCSV.py
+++ readCSV.py
@@ -4,12 +4,14 @@ import glob
 
 cwd = os.getcwd()
 folderPath = "test_data/"
-filename = "test_data1.csv"
+# filename = "test_data1.csv"
 
 def importData(filename):
+    # add exception error
     fullpath = os.path.join(cwd, folderPath, filename)
     # print(fullpath)
-    data = pd.read_csv(fullpath)
+    headers = ['time', 'voltage']
+    data = pd.read_csv(fullpath, names=headers)
     # [pd.read_csv(file) for file in glob.glob(fullpath)]
     # print(data)
     return data
