a6980931ffa991e44c7cf8bc7758bc4c2b21af91
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-22 02:04:04 -0400

    add the filter file

---
+++ filter_data.py
@@ -0,0 +1,29 @@
+
+from dataValidation import is_data_number
+from readCSV import importdata
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def filter_data(data):
+    voltage = data['voltage']
+    # remove low frequency data by applying fft and ifft
+    fft = np.fft.fft(voltage)
+    #plt.plot(fft[1500:11000])
+    for i in range(len(fft)):
+        if 1200 <= i <= 6500:
+            fft[i] = 0
+    filter_data = np.fft.ifft(fft)
+    filter_data = filter_data.real
+    #plt.plot(voltage[1:1000])
+    #plt.plot(data_remove_low[1:1000])
+    #plt.show()
+    return filter_data
+
+
+if __name__ == '__main__':
+    filename = "test_data1.csv"
+    test_data = importdata(filename)
+    data_valid = is_data_number(test_data)
+    filter_data(data_valid)
