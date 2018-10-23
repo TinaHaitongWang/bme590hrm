3ffc1ba765cc914571f5a207663bc9de74a65778
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-23 14:40:58 -0400

    fix the problem with new line for pep8

--- calculateMean_hr_bpm.py
+++ calculateMean_hr_bpm.py
@@ -3,17 +3,10 @@ import numpy as np
 
 def calculate_mean_hr_bpm(data, peaks):
     rr_interval_list = []
-    for i in range(len(peaks)-1):
-        upp = data.iloc[peaks[i+1]]['time']
+    for i in range(len(peaks) - 1):
+        upp = data.iloc[peaks[i + 1]]['time']
         low = data.iloc[peaks[i]]['time']
-        time = upp-low
+        time = upp - low
         rr_interval_list.append(time)
-
     mean_hr_bpm = np.mean(rr_interval_list)
     return mean_hr_bpm
-
-
-
-
-
-
