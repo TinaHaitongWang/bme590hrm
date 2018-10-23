08d6f8534753df410d610bf5bc6430fb9fbf8851
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-22 23:14:52 -0400

    update the peak_detection file

--- peak_detection.py
+++ peak_detection.py
@@ -1,103 +1,58 @@
 # this function perform peak detection
-from scipy.signal import find_peaks, fftconvolve, general_gaussian
+from scipy.signal import find_peaks, find_peaks_cwt
 import matplotlib.pyplot as plt
 from readCSV import importdata
 from dataValidation import is_data_number
 from filter_data import filter_data
 import numpy as np
-import pywavelets as pw
 
 
-def detect_peak(data):
-    '''
-
-    :param data:
-    :return:
+def detect_peak(data, min_voltage, is_secondary_peak):
+    plt.plot(data.voltage)
     max_voltage = max(data['voltage'])
     # find the moving average
     # data['rolling_mean_voltage'] = data['voltage'].rolling(window=50).mean()
-    hrw = 0.75
-    fs = 1 / (np.mean(np.diff(data.time)))
-    move_avg = data['voltage'].rolling(int(hrw*fs)).mean()
-    avg_sig = np.mean(data.voltage)
-    move_avg = [avg_sig if math.isnan(x) else x for x in move_avg]
-    data['moving_avg_voltage'] = [x*1.5 for x in move_avg]
-    min_voltage = np.mean(move_avg)
     range_voltage = [min_voltage, max_voltage]
     peaks, _ = find_peaks(data.voltage, height=range_voltage)
+    # correlation check for the
+    """"
+    signal_range = np.arange(peaks[0], peaks[1])
+    sample_signal = data.iloc[signal_range]['voltage']
+    corr = np.correlate(sample_signal, data.voltage, mode= "same")
+    threshold = np.correlate(sample_signal, sample_signal)
+    print(threshold)
+    peaks_corr, _ = find_peaks(corr)
+    plt.plot(peaks_corr, data.voltage[peaks_corr], "o")
+    """
+    if is_secondary_peak:
+        num = 1
+        real_peaks = []
+        for i in range(len(peaks)-1):
+            if i == 0:# first peak
+                if data.iloc[peaks[i]]['voltage'] > data.iloc[peaks[i+1]]['voltage']:
+                    # true local maxiumum
+                    real_peaks.append(peaks[i])
+                    num += 1
+            elif i == len(peaks)-1: # last peak
+                if real_peaks[-1] != peaks[-2] and data.iloc[peaks[-1]]['voltage'] > data.iloc[peaks[-2]]['voltage']:
+                    real_peaks[num] = peaks[-1]
 
-    plt.plot(data.voltage)
-    plt.plot(move_avg)
-    plt.plot(peaks, data.voltage[peaks], "o")
+            else:
+                if (data.iloc[peaks[i]]['voltage'] > data.iloc[peaks[i+1]]['voltage']) \
+                        and (data.iloc[peaks[i]]['voltage'] > data.iloc[peaks[i-1]]['voltage']):
+                    real_peaks.append(peaks[i])
+                    num += 1
+    else: real_peaks = peaks
+
+    #plt.plot(peaks, data.voltage[peaks], '*', color='red')
+    #plt.plot(real_peaks, data.voltage[real_peaks], "o")
     plt.show()
-
-    '''
-    # auto-correlation to fit the curve on the ROI region and then
-    # peak detection
-    plt.plot(data.voltage)
-    sample_rate = 1 / (np.mean(np.diff(data.time)))
-    max_bpm = 200
-    peak = QRS_detection(data.voltage, sample_rate, max_bpm)
-    plt.plot(peak)
-    plt.show()
-
-
-def QRS_detection(signal, sample_rate, max_bpm):
-    ## Stationary Wavelet Transform
-    coeffs = pw.swt(signal, wavelet="haar", level=2, start_level=0, axis=-1)
-    d2 = coeffs[1][1]  ##2nd level detail coefficients
-
-    ## Threhold the detail coefficients
-    avg = np.mean(d2)
-    std = np.std(d2)
-    sig_thres = [abs(i) if abs(i) > 2.0 * std else 0 for i in d2 - avg]
-
-    ## Find the maximum modulus in each window
-    window = int((60.0 / max_bpm) * sample_rate)
-    sig_len = len(signal)
-    n_windows = int(sig_len / window)
-    modulus, qrs = [], []
-
-    ##Loop through windows and find max modulus
-    for i in range(n_windows):
-        start = i * window
-        end = min([(i + 1) * window, sig_len])
-        mx = max(sig_thres[start:end])
-        if mx > 0:
-            modulus.append((start + np.argmax(sig_thres[start:end]), mx))
-
-    ## Merge if within max bpm
-    merge_width = int((0.2) * sample_rate)
-    i = 0
-    while i < len(modulus) - 1:
-        ann = modulus[i][0]
-        if modulus[i + 1][0] - modulus[i][0] < merge_width:
-            if modulus[i + 1][1] > modulus[i][1]:  # Take larger modulus
-                ann = modulus[i + 1][0]
-            i += 1
-
-        qrs.append(ann)
-        i += 1
-        ## Pin point exact qrs peak
-    window_check = int(sample_rate / 6)
-    # signal_normed = np.absolute((signal-np.mean(signal))/(max(signal)-min(signal)))
-    r_peaks = [0] * len(qrs)
-
-    for i, loc in enumerate(qrs):
-        start = max(0, loc - window_check)
-        end = min(sig_len, loc + window_check)
-        wdw = np.absolute(signal[start:end] - np.mean(signal[start:end]))
-        pk = np.argmax(wdw)
-        r_peaks[i] = start + pk
-
-    return r_peaks
-
-
+    return real_peaks
 
 
 if __name__ == '__main__':
-    filename = "test_data3.csv"
+    filename = "test_data1.csv"
     test_data = importdata(filename)
     data_valid = is_data_number(test_data)
     filtered_data = filter_data(data_valid)
-    detect_peak(filtered_data)
+    real_peaks = detect_peak(filtered_data, min_voltage=0, is_secondary_peak=False)
