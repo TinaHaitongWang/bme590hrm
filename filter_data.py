408d6fc0b9734d705b2f816427f3df2f14365306
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-23 14:26:39 -0400

    fix pep8 error

--- filter_data.py
+++ filter_data.py
@@ -3,11 +3,12 @@ from readCSV import importdata
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def filter_data(data):
     voltage = data['voltage']
     # remove low frequency data by applying fft and ifft
     fft = np.fft.fft(voltage)
-    #plt.plot(fft[1500:11000])
+    # plt.plot(fft[1500:11000])
     for i in range(len(fft)):
         if 1200 <= i <= 6500:
             # remove high frequency noise
@@ -22,4 +23,4 @@ if __name__ == '__main__':
     filename = "test_data20.csv"
     test_data = importdata(filename)
     data_valid = is_data_number(test_data)
-    filter_data(data_valid)
+    data = filter_data(data_valid)
