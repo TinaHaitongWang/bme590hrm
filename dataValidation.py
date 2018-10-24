4278fd7374feddda3eecce2a8b461f91c43db42d
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-23 22:50:07 -0400

    update data validation file to change data type to float

--- dataValidation.py
+++ dataValidation.py
@@ -3,17 +3,23 @@ import pandas as pd
 
 
 def is_data_number(test_data):
-    # remove nan/ empty
     # print(test_data)
     new_data = test_data[pd.to_numeric(test_data.time,
                                        errors='coerce').notnull()]
-    test_data2 = new_data[pd.to_numeric(new_data.voltage,
-                                        errors='coerce').notnull()]
-    # print(test_data2)
-    return test_data2
+    out_data = new_data[pd.to_numeric(new_data.voltage,
+                                      errors='coerce').notnull()]
+
+    if out_data.time.dtype == 'object':
+        out_data = out_data.copy()
+        out_data.loc[:, 'time'] = out_data['time'].apply(pd.to_numeric,
+                                                         errors='ignore')
+    if out_data.voltage.dtype == 'object':
+        out_data.loc[:, 'voltage'] = out_data['voltage'].apply(pd.to_numeric,
+                                                               errors='ignore')
+    return out_data
 
 
 if __name__ == '__main__':
-    filename = "test_data32.csv"
+    filename = "test_data30.csv"
     data = importdata(filename)
     is_data_number(data)
