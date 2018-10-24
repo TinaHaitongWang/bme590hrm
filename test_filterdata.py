12484870001aec6dc899f9bce485ec81b17df247
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-24 00:21:41 -0400

    fix test method

--- test_filterdata.py
+++ test_filterdata.py
@@ -4,13 +4,12 @@ from dataValidation import is_data_number
 import numpy as np
 import random
 
-filename = "test_data28.csv"
-test_data = importdata(filename)
-data_valid = is_data_number(test_data)
-data = filter_data(data_valid)
 
-
-def test_is_filter_data_valid(source):
+def test_is_filter_data_valid():
+    filename = "test_data28.csv"
+    test_data = importdata(filename)
+    data_valid = is_data_number(test_data)
+    source = filter_data(data_valid)
     sample = len(source)
     noise = 0.000000000000008*np.asarray(random.sample(range(0, sample), sample))
     source.voltage = source.voltage + noise
