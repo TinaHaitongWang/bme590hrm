48da05fa94acc8359e8d5572c53c70e21eae949d
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-25 15:29:05 -0400

    add the file to calculate the number of beats and beats time

---
+++ calculate_beats_number_and_beats.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def calculate_number_beats_and_bests(data, peaks):
+    num_beats = len(peaks)
+    beats_list = []
+    for i in range(len(peaks) - 1):
+        beat = data.iloc[peaks[i]]['time']
+        beats_list.append(beat)
+
+    beats = np.array(beats_list)
+    return num_beats, beats
+
+
+
+
