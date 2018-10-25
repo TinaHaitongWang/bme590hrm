ff4d551871bc5f2f4c85a9d164c845121e62192d
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-25 00:32:09 -0400

    add the file to compute duration and coltage extreme

---
+++ calculate_voltage_extreme_and_duration.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def calculate_voltage_extreme(data):
+    max_voltage = np.max(data.voltage)
+    min_voltage = np.min(data.voltage)
+    return max_voltage, min_voltage
+
+
+def calculate_duration(data):
+    return data['time'].iloc[-1]
