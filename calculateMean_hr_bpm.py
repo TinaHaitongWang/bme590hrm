b76e60957c74716d25147ee67aa8de37cb2d5bb4
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-24 23:36:47 -0400

    update to calculate the bpm

--- calculateMean_hr_bpm.py
+++ calculateMean_hr_bpm.py
@@ -8,5 +8,15 @@ def calculate_mean_hr_bpm(data, peaks):
         low = data.iloc[peaks[i]]['time']
         time = upp - low
         rr_interval_list.append(time)
-    mean_hr_bpm = np.mean(rr_interval_list)
+    beats_in_seconds = np.mean(rr_interval_list)
+    mean_hr_bpm = 60 / beats_in_seconds
     return mean_hr_bpm
+
+
+# if __name__ == '__main__':
+#     filename = "test_data3.csv"
+#     test_data = importdata(filename)
+#     data_valid = is_data_number(test_data)
+#     filtered_data = filter_data(data_valid)
+#     peaks_real = detect_peak(filtered_data)
+#     mean_bpm = calculate_mean_hr_bpm(filtered_data, peaks_real)
