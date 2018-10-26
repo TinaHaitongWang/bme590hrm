259c09b225c10a6f1b6390931044d7348913289a
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-25 23:25:03 -0400

    to fix json implementation

--- metrics_dictionary.py
+++ metrics_dictionary.py
@@ -5,11 +5,10 @@ import os
 def create_dictionary(data):
     metrics = {"Filename": data.filename,
                "Mean HR BPM": data.mean_hr_bpm,
-               "Voltage Extreme": data.voltage_extreme,
-               "Duration": data.time_duration,
+               "Voltage Extreme (mv)": data.voltage_extreme,
+               "Duration (s)": data.time_duration,
                "Number of Beats": data.num_beats,
-               "Beats": data.beats}
-
+               "Beats time interval": data.beats}
     output_metrics_to_json(metrics)
 
 
@@ -21,10 +20,9 @@ def output_metrics_to_json(metrics):
 
     suffix = '.json'
     outpath = os.path.join(dirpath, metrics["Filename"] + suffix)
-    json_metrics = json.dumps(dict)
-    write_file = open(outpath, "w")
-    write_file.write(json_metrics)
-    write_file.close()
+    with open(outpath, 'w') as fp:
+        json.dump(metrics, fp, sort_keys=False, indent=4)
+
 
 
 
