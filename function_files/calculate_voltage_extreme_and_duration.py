6b5e3bb5d978e1a4e26f531a91288a5bc77cf06f
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-26 21:45:13 -0400

    add sphinx friendly docstring comment

--- function_files/calculate_voltage_extreme_and_duration.py
+++ function_files/calculate_voltage_extreme_and_duration.py
@@ -1,11 +1,27 @@
 import numpy as np
+"""This is a calculation function file 
+
+    Author: Haitong Wang (Tina)
+
+"""
 
 
 def calculate_voltage_extreme(data):
+    """
+    this function calculate the maximum and minimum voltage
+    with the ecg data
+    :param data: ecg data with time and voltage
+    :return: maximum and minimum voltage
+    """
     max_voltage = np.max(data.voltage)
     min_voltage = np.min(data.voltage)
     return max_voltage, min_voltage
 
 
 def calculate_duration(data):
+    """
+    this function calculates the time span of ecg data
+    :param data: ecg data with time and voltage
+    :return: total time of the data
+    """
     return data['time'].iloc[-1]
