0ba5273a461ce696fd9ccf7d885c4a5a240c7c09
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-26 21:15:30 -0400

    add sphinx friendly docstrings

--- function_files/calculate_beats_number_and_beats.py
+++ function_files/calculate_beats_number_and_beats.py
@@ -1,7 +1,20 @@
 import numpy as np
+"""This is a calculation function file 
+
+   Author: Haitong Wang (Tina)
+
+"""
 
 
 def calculate_number_beats_and_bests(data, peaks):
+    """
+    calculate_number_beats_and_beats function calculates
+    number of beats in the ecg data and the corresponding
+    time
+    :param data: ecg data with time and voltage
+    :param peaks: index of peak location in the ecg data
+    :return: number of beats and time of beats occur
+    """
     num_beats = len(peaks)
     beats_list = []
     for i in range(len(peaks)):
