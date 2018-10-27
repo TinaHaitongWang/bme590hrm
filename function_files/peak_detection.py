60558d58535429c811c630e6897c602eef4768e6
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-26 22:03:54 -0400

    fix pep8 trailing whitespace

--- function_files/peak_detection.py
+++ function_files/peak_detection.py
@@ -4,10 +4,7 @@ from function_files.dataValidation import is_data_number
 from function_files.filter_data import filter_data
 import numpy as np
 """This is a function file 
-
-    Author: Haitong Wang (Tina)
-
-"""
+Author: Haitong Wang (Tina)"""
 
 
 def detect_peak(section_data):
