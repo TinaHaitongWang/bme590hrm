6aeb84a80b0a322e885b9973536d056951dca8f7
Haitong Wang <tinahaitongwang@Haitongs-MacBook-Pro.local>
2018-10-26 21:51:44 -0400

    add sphinx friendly docstrings

--- function_files/readCSV.py
+++ function_files/readCSV.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import os
+"""This is a function file 
 
+    Author: Haitong Wang (Tina)
 
+"""
+""" if the test data is not save in the source path, you
+need to update it in the following code 
+"""
 cwd = os.getcwd()
 folderPath = "test_data"
 
 
 def importdata(file_name):
+    """
+    this function import data into a dataframe with time and
+    voltage
+    :param file_name: test data name
+    :return: dataframe
+    """
     # add exception error
     fullpath = os.path.join(cwd, folderPath, file_name)
     # print(fullpath)
